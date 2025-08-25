813ccd97f383fba42c41129a95491830bceed0bd
dolgion11 <m.ariundolgion@gmail.com>
2025-08-25 17:04:17 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+# app.py
+import streamlit as st
+
+st.set_page_config(page_title="JET App Suite", page_icon="📊", layout="wide")
+
+st.title("JET App Suite")
+st.write("Зүүн талын **Pages** хэсгээс сонгоод аппаа ажиллуулна уу.")
